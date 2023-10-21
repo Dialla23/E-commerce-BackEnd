cd709c3bbe8b2237a86c840c847c9de96057d96b
Dialla23 <diallomahamoudou39@yahoo.fr>
2023-10-21 00:14:07 -0300

    models added

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,16 +5,31 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+})
 
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+})
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(ProductTag, {
+  through:
+    model: ProductTag,
+  unique: false
+})
+  // Tags belongToMany Products (through ProductTag)
+  Tag.belongsToMany(ProductTag, {
+    through:
+      model: ProductTag,
+    unique: false
+  })
 
-// Tags belongToMany Products (through ProductTag)
-
-module.exports = {
-  Product,
-  Category,
-  Tag,
-  ProductTag,
-};
+  module.exports = {
+    Product,
+    Category,
+    Tag,
+    ProductTag,
+  };
--- Develop/server.js
+++ Develop/server.js
@@ -2,12 +2,14 @@ const express = require('express');
 const routes = require('./routes');
 require('dotenv').config();
 // import sequelize connection
-
+const { Sequelize, DataTypes } = require('sequelize');
+// const sequelize = new Sequelize('sqlite::memory:');
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   // Sequelize configuration options
   host: 'localhost',
     dialect: 'mysql',
 });
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
